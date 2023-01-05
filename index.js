78fd3c57984fedf04b9d3819a6de832b49a97357
Wesley <wesleymichaelpereira@hotmail.com>
2023-01-05 04:22:42 -0300

    Seleção de item - 1 função

--- index.js
+++ index.js
@@ -1,31 +1,13 @@
 
-function selecionarPrato(pratoEscolhido) {
-    const pratoAnterior = document.querySelector('.prato .selecionado');
-    if (pratoAnterior !== null){
-        pratoAnterior.classList.remove('selecionado');
+function selecionarItem(item, itemEscolhido) {
+    const itemAnterior = document.querySelector(item + ' .selecionado');
+    if (itemAnterior !== null){
+        itemAnterior.classList.remove('selecionado');
     }
 
-    const pratoSelecionado = document.querySelector(pratoEscolhido);
+    const pratoSelecionado = document.querySelector(itemEscolhido);
     pratoSelecionado.classList.add('selecionado'); 
 }
 
-function selecionarBebida (bebidaEscolhida){
-    const bebidaAnterior = document.querySelector('.bebida .selecionado');
-    if (bebidaAnterior !== null){
-        bebidaAnterior.classList.remove('selecionado');
-    }
 
-    const bebidaSelecionada = document.querySelector(bebidaEscolhida);
-    bebidaSelecionada.classList.add('selecionado');
-}
-
-function selecionarSobremesa (sobremesaEscolhida){
-    const sobremesaAnterior = document.querySelector('.sobremesa .selecionado');
-    if (sobremesaAnterior !== null){
-        sobremesaAnterior.classList.remove('selecionado');
-    }
-
-    const sobremesaSelecionada = document.querySelector(sobremesaEscolhida);
-    sobremesaSelecionada.classList.add('selecionado')
-}
 
